9b06a3950eeebfae6b077de77b831d8febbe7af1
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-09-13 13:10:47 +0530

    Refactor hello controller to return "Hello!" instead of "hello"

--- src/main/java/com/vit/carpool/controllers/hello.java
+++ src/main/java/com/vit/carpool/controllers/hello.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class hello {
     @GetMapping("/hello")
-    public String hello(){
-        return "hello";
+    public String hello() {
+        return "Hello!";
     }
 }
