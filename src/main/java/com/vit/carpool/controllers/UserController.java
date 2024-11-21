1539efb93c564718ad46238088e1fb4b09a79f82
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-11-21 14:10:43 +0530

    Refactor: Add endpoint to get user by ID

--- src/main/java/com/vit/carpool/controllers/UserController.java
+++ src/main/java/com/vit/carpool/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.vit.carpool.controllers;
 import com.vit.carpool.entities.User;
 import com.vit.carpool.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,16 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> UserId(@PathVariable String id) {
+        try {
+            User result = userService.getUser(id);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error: " + e.getMessage());
+        }
+    }
 
     @PostMapping("/signin")
     public ResponseEntity<String> signIn(@RequestBody User user) {
