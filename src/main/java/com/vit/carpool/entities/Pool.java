6bf97a9a0c6822a24ecfd635fc4047a4787e1f11
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-11-21 14:10:51 +0530

    Refactor: Add auto-generated ID for Pool entity

--- src/main/java/com/vit/carpool/entities/Pool.java
+++ src/main/java/com/vit/carpool/entities/Pool.java
@@ -6,7 +6,6 @@ import java.time.LocalTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -18,6 +17,7 @@ import jakarta.persistence.Column;
 public class Pool {
 
     @Id
+    @GeneratedValue
     private Long poolID;
 
     private String source;
