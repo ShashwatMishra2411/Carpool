7960e8d0fc2cb9409da6880708e3c0bdcf2db666
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-12-16 10:31:08 +0530

    Add RemoveUser entity to handle user removal from a pool

---
+++ src/main/java/com/vit/carpool/entities/RemoveUser.java
@@ -0,0 +1,23 @@
+package com.vit.carpool.entities;
+
+public class RemoveUser {
+    private String userid;
+    private long poolid;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public long getPoolid() {
+        return poolid;
+    }
+
+    public void setPoolid(long poolid) {
+        this.poolid = poolid;
+    }
+
+}
